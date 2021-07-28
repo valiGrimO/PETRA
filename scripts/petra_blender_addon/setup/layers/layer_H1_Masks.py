1fb1c18a98fac0d77dbec698fa9de885ec3f64d3
valiGrimO <valentin.grimaud@univ-nantes.fr>
2021-07-28 08:31:35 +0200

    script to create a layer - WIP

---
+++ scripts/petra_blender_addon/setup/layers/layer_H1_Masks.py
@@ -0,0 +1,22 @@
+import bpy
+
+MATERIALS:
+    to selected object, apply material c1_prv
+# how to differenciate prt to prv?
+
+
+COMPOSITING:
+    connect Render Layers with C1
+    disconnect Render Layers from H1
+    disconnect Render Layers from H2
+    disconnect Render Layers from L1
+    disconnect Render Layers from R1
+    disconnect Render Layers from R2
+    disconnect Render Layers from R3
+    disconnect Render Layers from R4
+    disconnect Render Layers from R5
+    disconnect Render Layers from R6
+    
+
+# Reference Sphere
+bpy.data.objects["Reference Sphere"].hide_render = False
