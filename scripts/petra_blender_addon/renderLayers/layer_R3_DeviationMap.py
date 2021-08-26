655754fd905b26eb20a63f1e0c0c65da016fb108
valiGrimO <valentin.grimaud@univ-nantes.fr>
2021-08-26 17:00:35 +0200

    Integration of Deviation Map

--- scripts/petra_blender_addon/renderLayers/layer_R3_DeviationMap.py
+++ scripts/petra_blender_addon/renderLayers/layer_R3_DeviationMap.py
@@ -7,23 +7,40 @@ bpy.context.scene.render.engine = 'BLENDER_EEVEE'
     # For selected objects:
         # Apply `r3_deviation_map`
 
-# Reset Outputs connexions
-    # remove every connexion from `File Output` nodes
+# Configure Compositor
+    # Connect `Render Layers/[0]` to `Hub/[6]`
 
 #################################################
+*This part is written in MarkDown:*
+
+We have to configure some options through the interface of the plugin. We need to match:
+1. the vertex group available (`col`, `DM1`, `DM2`)
+2. with extreme values
+
+Those options are to be configured in the `r3_deviation_map` material.
+
+*End of the MarkDown passage.*
+#################################################
+
 # DM1
-# Configure Compositor
-    # Connect `Render Layers/[0]` to `Hub/[3]`
+    # set the DM1 configuration 
 
+    # unmute `File Output`
+    # mute `File Output.001`
+    # unmute `File Output.002`
+    # mute `File Output.003`
 
-
-# Produce Documentation
     # hit "produce documentation" in the PETrA Pannel (Rendering)
 
+# DM2
+    # set the DM1 configuration 
 
+    # mute `File Output`
+    # unmute `File Output.001`
+    # mute `File Output.002`
+    # unmute `File Output.003`
 
-
-
+    # hit "produce documentation" in the PETrA Pannel (Rendering)
 
 # Set Compositor in its initial state
-    # Deconnect `Render Layers/[0]` to `Hub/[3]`
+    # Deconnect `Render Layers/[0]` to `Hub/[6]`
