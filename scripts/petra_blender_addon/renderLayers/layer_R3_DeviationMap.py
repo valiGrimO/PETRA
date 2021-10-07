99fcf486ee42efb7a00f38d6f8a48653f38b861a
valiGrimO <valentin.grimaud@univ-nantes.fr>
2021-10-07 09:54:55 +0200

    Change output pattern of R3

--- scripts/petra_blender_addon/renderLayers/layer_R3_DeviationMap.py
+++ scripts/petra_blender_addon/renderLayers/layer_R3_DeviationMap.py
@@ -8,6 +8,7 @@ nodetree = C.scene.node_tree
 
 node_RL = S.node_tree.nodes["Render Layers"]
 node_PETrA = S.node_tree.nodes["PETrA"]
+node_R3 = node_PETrA.node_tree.nodes["R3_deviationMap"]
 
 # Select render Engine
 C.scene.render.engine = "BLENDER_EEVEE"
@@ -19,6 +20,11 @@ selected_object.material_slots[0].material = material
 
 # Configure Compositor
 nodetree.links.new(node_RL.outputs[0], node_PETrA.inputs[6])
+
+# Adjust Output filenames (end with -[normValue])
+node_R3_Output = node_R3.node_tree.nodes["File Output"]
+pattern = f"Cam-##_R3_{R3_normValue}"
+node_R3_Output.file_slots[0].path = pattern
 
 # Change vertex color in the material
 D.materials["r3_deviation_map"].node_tree.nodes["Vertex Color"].layer_name = "DM1"
