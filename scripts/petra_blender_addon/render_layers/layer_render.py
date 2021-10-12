6936a629b3092c00d711089a0c96b8294ce44e13
valiGrimO <valentin.grimaud@univ-nantes.fr>
2021-10-12 11:12:41 +0200

    change "render animation" to "select camera, then render image"

--- scripts/petra_blender_addon/render_layers/layer_render.py
+++ scripts/petra_blender_addon/render_layers/layer_render.py
@@ -1,2 +1,6 @@
 import bpy
-bpy.ops.render.render(animation=True)
+
+for obj in bpy.data.objects:
+    if obj.type == 'CAMERA':
+    	bpy.ops.petra.activate_and_preview_scene_camera()
+    	bpy.ops.render.render(use_viewport=True)
