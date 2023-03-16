b7e8143a4d578434e2db7eb69a47ee272c172bca
valiGrimO <87708166+valiGrimO@users.noreply.github.com>
2023-03-16 16:56:40 +0100

    Version 0.4

--- scripts/petra_blender_addon/render_layers/layer_R5_Aspect.py
+++ scripts/petra_blender_addon/render_layers/layer_R5_Aspect.py
@@ -13,7 +13,7 @@ node_PETrA = S.node_tree.nodes["PETrA"]
 C.scene.render.engine = "BLENDER_EEVEE"
 
 # Apply material
-material = D.materials["r5_aspect"]
+material = D.materials["R5_ASP"]
 selected_object = C.selected_objects[0]
 selected_object.material_slots[0].material = material
 
