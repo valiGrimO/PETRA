88e0e155ae39c3b3bc8538eb2a3cc7cd6423ba50
valiGrimO <87708166+valiGrimO@users.noreply.github.com>
2023-03-16 17:02:28 +0100

    Version 0.4

---
+++ scripts/petra_blender_addon/render_layers/layer_R2_CL1.py
@@ -0,0 +1,46 @@
+import bpy
+from petra_blender_addon import popup
+from petra_blender_addon.blendertools import remove_link
+
+
+# Get reference
+C = bpy.context
+D = bpy.data
+S = D.scenes["Scene"]
+nodetree = C.scene.node_tree
+
+node_RL = S.node_tree.nodes["Render Layers"]
+node_PETrA = S.node_tree.nodes["PETrA"]
+node_R2 = node_PETrA.node_tree.nodes["R2_contourLine"]
+node_R2_nodetree = node_R2.node_tree
+node_R2b = node_R2.node_tree.nodes["ColorRamp"]
+node_R2z = node_R2.node_tree.nodes["File Output"]
+node_R2_tmp = bpy.data.node_groups["R2: Contour Lines"].nodes["tmp_CL"]
+
+
+# Select render Engine
+C.scene.render.engine = "BLENDER_EEVEE"
+
+
+# Apply material to selected objects
+material = D.materials["R2_CL"]
+selected_object = C.selected_objects[0]
+selected_object.material_slots[0].material = material
+
+
+# Apply material to every selected object
+bpy.ops.object.make_links_data(type='MATERIAL')
+
+# Apply the right spacing
+D.materials["R2_CL"].node_tree.nodes["math1"].mute = False
+D.materials["R2_CL"].node_tree.nodes["math2"].mute = True
+D.materials["R2_CL"].node_tree.nodes["math3"].mute = True
+
+
+# Output name
+node_R2z.file_slots[0].path = "Cam-##_R2_CL1"
+node_R2_tmp.file_slots[0].path = "Cam-##_R2_CL1"
+
+    
+# Configure Compositor
+nodetree.links.new(node_RL.outputs[0], node_PETrA.inputs[5])
