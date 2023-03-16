6d51f323035f3afcb2440850cc4f1aa48d5ad826
valiGrimO <87708166+valiGrimO@users.noreply.github.com>
2023-03-16 16:59:52 +0100

    Version 0.4

--- scripts/petra_blender_addon/render_layers/layer_R3_DeviationMap.py
+++ scripts/petra_blender_addon/render_layers/layer_R3_DeviationMap.py
@@ -14,7 +14,7 @@ node_R3 = node_PETrA.node_tree.nodes["R3_deviationMap"]
 C.scene.render.engine = "BLENDER_EEVEE"
 
 # Apply material to selected objects
-material = D.materials["r3_deviation_map"]
+material = D.materials["R3_DM"]
 selected_object = C.selected_objects[0]
 selected_object.material_slots[0].material = material
 
@@ -27,16 +27,26 @@ nodetree.links.new(node_RL.outputs[0], node_PETrA.inputs[6])
 # Adjust Output filenames
 node_R3_Output0 = node_R3.node_tree.nodes["File Output"]
 node_R3_Output1 = node_R3.node_tree.nodes["File Output.001"]
-id_DM = D.materials["r3_deviation_map"].node_tree.nodes["Vertex Color"].layer_name
-node_R3_Output0.file_slots[0].path = f"Cam-##_R3_{id_DM}"
-node_R3_Output1.file_slots[0].path = f"Cam-##_R3_{id_DM}_BBR"
-node_R3_Output1.file_slots[1].path = f"Cam-##_R3_{id_DM}_BVJR"
-node_R3_Output1.file_slots[2].path = f"Cam-##_R3_{id_DM}_MAGMA"
-node_R3_Output1.file_slots[3].path = f"Cam-##_R3_{id_DM}_SPECTRAL"
-node_R3_Output1.file_slots[4].path = f"Cam-##_R3_{id_DM}_VIRIDIS"
-node_R3_Output1.file_slots[5].path = f"Cam-##_H1_{id_DM}"
 
-# Change vertex color in the material
-# D.materials["r3_deviation_map"].node_tree.nodes["Vertex Color"].layer_name = "DM1"
+R3_ID_full = D.materials["R3_DM"].node_tree.nodes["inputColor"].layer_name
+R3_ID = R3_ID_full[0:3]
 
-# Note: Since several Vertex Color Indexes beginning with `DM` might exist (max = 7), then a loop have to render everyone, and the output name should be incremented (`DM1`, `DM2`, etc.)
+node_R3_Output0.file_slots[0].path = f"Cam-##_R3_{R3_ID}"
+node_R3_Output1.file_slots[0].path = f"Cam-##_R3_{R3_ID_full}_NDG"
+node_R3_Output1.file_slots[1].path = f"Cam-##_R3_{R3_ID_full}_BBR"
+node_R3_Output1.file_slots[2].path = f"Cam-##_R3_{R3_ID_full}_BVJR"
+node_R3_Output1.file_slots[3].path = f"Cam-##_R3_{R3_ID_full}_MAGMA"
+node_R3_Output1.file_slots[4].path = f"Cam-##_R3_{R3_ID_full}_SPECTRAL"
+node_R3_Output1.file_slots[5].path = f"Cam-##_R3_{R3_ID_full}_VIRIDIS"
+node_R3_Output1.file_slots[6].path = f"Cam-##_R3_{R3_ID}_H1"
+
+
+
+
+
+#################################################
+# tmp
+tmpR3 = bpy.data.node_groups["R3: Deviation Map"].nodes["tmp_R3"]
+
+tmpR3.file_slots[0].path = f"Cam-##_R3_{R3_ID}_MAGMA"
+tmpR3.file_slots[1].path = f"Cam-##_R3_{R3_ID}_H1"
